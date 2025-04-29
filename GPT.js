aaf0c2e6bb61cf41c9b111a727d63a571e92ba21
Muhammadjon5554 <muhammadjonnematullayev@gmail.com>
2025-04-29 16:14:33 +0500

    kutubhonam

--- GPT.js
+++ GPT.js
@@ -2,10 +2,7 @@ import express from "express";
 import OpenAI from "openai";
 const app = express();
 
-const client = new OpenAI({
-  apiKey:
-    "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",
-});
+import client from "client";
 
 async function superAi() {
   const response = await client.responses.create({
